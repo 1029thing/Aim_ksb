c38b65e8c5a88d57130d38f32899a2a07330f29e
MinSeok3796 <ms3796@naver.com>
2024-04-15 06:59:21 +0900

    챌린지 화면 이동

--- app/src/main/java/com/example/myapplication/ui/challenge/ChallengeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/challenge/ChallengeFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.challenge;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplication.Activity.ChallengeApply;
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentChallengeBinding;
 
@@ -25,6 +27,16 @@ public class ChallengeFragment extends Fragment {
 
         binding = FragmentChallengeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        binding.firstCh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(), ChallengeApply.class);
+                startActivity(intent);
+                getActivity().overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
+
 
 
         return root;
